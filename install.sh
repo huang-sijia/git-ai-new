1e7ddf4fef5428ff3d1e1a1209b2c97cefd139b8
sijia.huang <sijia.huang@gl-inet.com>
2026-04-24 18:31:31 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 curl -sSL https://usegitai.com/install.sh | bash
-
-if [ !-d '~/.git-ai/bin/' ]
+wget  https://github.com/huang-sijia/git-ai-new/releases/download/release1/git-ai
+if [ -d "~/.git-ai/bin/" ]
 then
 	echo "没有找到git-ai文件夹，请先确认是否安装git-ai成功"
 else
-
+	killall git-ai
+	chmod +x ./git-ai	
 	cp ./git-ai ~/.git-ai/bin/
+	echo "更新为GL的git-ai成功"
 fi
